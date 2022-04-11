f0bd1c524cbd6b3b27f2a34e10c4b1c66e69f563
Sonic <sf7293gv@go.minnstate.edu>
2022-04-11 01:21:59 -0500

    Created a pager and an adapter to switch between the 7 fragments in the app, also modified the design to show it in the app

--- app/src/main/java/com/example/hydration/MainActivity.kt
+++ app/src/main/java/com/example/hydration/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.hydration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var waterViewPager: ViewPager2
+    private lateinit var waterTabLayout: TabLayout
 
     private val waterViewModel by lazy {
         WaterViewModelFactory((application as HydrationApplication).repository).create(WaterViewModel::class.java)
@@ -13,6 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        waterViewPager = findViewById(R.id.water_view_pager)
+        waterTabLayout = findViewById(R.id.water_days_tablayout)
+
+        val days = resources.getStringArray(R.array.days)
+
+        val pagerAdapter = WaterViewPagerAdapter(this, days)
+        waterViewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(waterTabLayout, waterViewPager) { tab, position ->
+            tab.text = days[position]
+        }.attach()
 
 //        val tuesday = WaterRecord("Tuesday", 4)
 //        waterViewModel.insertNewRecord(tuesday)
@@ -23,7 +41,8 @@ class MainActivity : AppCompatActivity() {
             Log.d("MAIN_ACTIVITY", "$records")
         }
 
-        supportFragmentManager.beginTransaction().add(R.id.content, HydrationFragment.newInstance("Tuesday")).commit()
+//        supportFragmentManager.beginTransaction().add(R.id.content, HydrationFragment.newInstance("Tuesday")).commit()
+
 
     }
 }
